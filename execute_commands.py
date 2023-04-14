847505a519ae3c55cad5e0ced9f2fbf390e68116
mao <rxm603@case.edu>
2023-04-14 10:36:22 -0500

    script which can auto run the baseline at server

---
+++ execute_commands.py
@@ -0,0 +1,17 @@
+import subprocess
+import time
+
+
+def execute_commands_from_file(file_path):
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+
+    for command in lines:
+        print(f"Executing command: {command.strip()}")
+        subprocess.run(command, shell=True, check=True, text=True)
+        print(f"Finished executing command. Waiting for 30 minutes...")
+        time.sleep(30 * 60)  # Wait for 30 minutes
+
+
+if __name__ == "__main__":
+    execute_commands_from_file("baseline.txt")
