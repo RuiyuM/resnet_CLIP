05a34b1aecd99137c617bcbb7e593a4a2770b7f6
maomao <rxm603@case.edu>
2023-04-27 14:32:03 -0400

    test_graph

--- test_code.py
+++ test_code.py
@@ -1,12 +1,15 @@
-from collections import Counter
+import pickle
 
+file_name = "temperature_resnet18_Tiny-Imagenet_known40_init8_batch1500_seed1_certainty_unknown_T0.5_known_T0.5_modelB_T1.0.pkl"
 
-a  = [2,2,3,4,23,1234,1232,3,4,3,3]
+# Add the directory to the file path
+file_path = f"log_AL/{file_name}"
 
+# Open the file and load the data
+with open(file_path, 'rb') as f:
+    data = pickle.load(f)
+    max_min_temperature_acc = [data['Acc'][i] for i in data['Acc']]
+    max_min_temperature_precision = [data['Precision'][i] for i in data['Precision']]
+    max_min_temperature_recall = [data['Recall'][i] for i in data['Recall']]
 
-x = Counter(a)
-#print (x)
-top_1 = x.most_common(1)
-
-
-print (top_1)
+print(max_min_temperature_acc)
