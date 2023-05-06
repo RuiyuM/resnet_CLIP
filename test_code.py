f41acaadef5f9573f8a596e2ea74764df5f4302d
maomao <rxm603@case.edu>
2023-05-06 13:09:36 -0400

    update passive query

--- test_code.py
+++ test_code.py
@@ -2,14 +2,12 @@ import os
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-
 #%%
-sampling_methods = ['random', 'uncertainty', "BGADL", "OpenMax", "Core_set", 'certainty', 'active_query', 'AV_temperature']
+sampling_methods = ['random', 'uncertainty', "BGADL", "OpenMax", "Core_set", 'certainty', 'active_query', 'AV_temperature', "test_query"]
 datasets = {'Tiny-Imagenet': {'init_percent': 8, 'known_class': [40], 'batch': [200, 400, 600, 1500]},
             'cifar100': {'init_percent': 8, 'known_class': [20], 'batch': [200, 400, 600, 1500]},
             'cifar10': {'init_percent': 1, 'known_class': [2], 'batch': [200, 400, 600, 1500]}}
-
-
+#%%
 def load_pkl_files(dataset_name, known_class, batch_size=None):
     pkl_files = {method: [] for method in sampling_methods}
     log_al_folder = 'log_AL/'
@@ -19,17 +17,12 @@ def load_pkl_files(dataset_name, known_class, batch_size=None):
                 continue
             for method in sampling_methods:
                 if method in file:
+                    if "_per_round_query_index" in file:
+                        continue
                     pkl_files[method].append(os.path.join(log_al_folder, file))
                     break
     return pkl_files
-
-
-# The rest of the code remains the same
-
 #%%
-
-
-
 def plot_graphs(group_name, acc_list, precision_list, recall_list, acc_std_list, precision_std_list, recall_std_list, batch_size):
     query_numbers = list(range(len(acc_list[0])))
 
@@ -42,8 +35,7 @@ def plot_graphs(group_name, acc_list, precision_list, recall_list, acc_std_list,
     plt.ylabel('Accuracy')
     plt.title(f'Accuracy for {group_name} Batch {batch_size}')
     plt.legend()
-    plt.savefig(f"{group_name}_accuracy_batch{batch_size}.png")
-    plt.close()
+    plt.show()
 
     plt.figure()
     for i, (precision, precision_std) in enumerate(zip(precision_list, precision_std_list)):
@@ -54,8 +46,7 @@ def plot_graphs(group_name, acc_list, precision_list, recall_list, acc_std_list,
     plt.ylabel('Precision')
     plt.title(f'Precision for {group_name} Batch {batch_size}')
     plt.legend()
-    plt.savefig(f"{group_name}_Precision_batch{batch_size}.png")
-    plt.close()
+    plt.show()
 
     plt.figure()
     for i, (recall, recall_std) in enumerate(zip(recall_list, recall_std_list)):
@@ -66,24 +57,8 @@ def plot_graphs(group_name, acc_list, precision_list, recall_list, acc_std_list,
     plt.ylabel('Recall')
     plt.title(f'Recall for {group_name} Batch {batch_size}')
     plt.legend()
-    plt.savefig(f"{group_name}_Recall_batch{batch_size}.png")
-    plt.close()
-
-def print_proposed_results(files, method='test_query', dataset_name='', batch_size=0):
-    for file in files:
-        with open(file, 'rb') as f:
-            data = pickle.load(f)
-            seed = file.split('_')[-2]
-            print(f"Results for {dataset_name} dataset, batch size {batch_size}, Seed {seed} - {method}:")
-            print("Accuracy: ", data['Acc'])
-            print("Precision: ", data['Precision'])
-            print("Recall: ", data['Recall'])
-            print()
-
-
-
-
-
+    plt.show()
+#%%
 for dataset_name, dataset_info in datasets.items():
     for known_class in dataset_info['known_class']:
         for batch_size in dataset_info['batch']:
@@ -94,8 +69,6 @@ for dataset_name, dataset_info in datasets.items():
 
             for method, files in pkl_files_dict.items():
                 acc_vals, precision_vals, recall_vals = [], [], []
-                if method == 'test_query':
-                    print_proposed_results(files, method, dataset_name, batch_size=1500)
                 for file in files:
                     with open(file, 'rb') as f:
                         data = pickle.load(f)
@@ -121,18 +94,5 @@ for dataset_name, dataset_info in datasets.items():
                     precision_std_list.append(precision_std)
                     recall_std_list.append(recall_std)
 
-                    if method == 'OpenMax':
-                        print(f"OpenMax Results for {dataset_name} dataset, Known {known_class}, Batch size {batch_size}:")
-                        print("Accuracy: ", acc_avg)
-                        print("Precision: ", precision_avg)
-                        print("Recall: ", recall_avg)
-                        print()
-
             plot_graphs(group_name, acc_list, precision_list, recall_list, acc_std_list, precision_std_list,
                         recall_std_list, batch_size)
-
-
-
-
-
-
